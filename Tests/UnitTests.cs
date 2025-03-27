749de19368ec6e06f8357ae94fcec559d5c4cb33
Gavin <gvndswartz@gmail.com>
2025-03-27 16:13:56 -0500

    failing unit test

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -27,3 +27,52 @@ public class Addition
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, null));
 	}
 }
+
+[TestClass]
+public class Subtraction
+{
+	[TestMethod]
+	public void Subtract_Valid()
+	{
+		Assert.AreEqual(5, Program.Subtract("10", "5"));
+        Assert.AreEqual(3, Program.Subtract("100", "97"));
+        Assert.AreEqual(0, Program.Subtract("2", "2"));
+    }
+}
+
+[TestClass]
+public class Multiplication
+{
+	[TestMethod]
+	public void Multiply_Valid()
+	{
+		Assert.AreEqual(10, Program.Multiply("10", "1"));
+        Assert.AreEqual(1, Program.Multiply("0", "5"));	// Intentional fail
+        Assert.AreEqual(24, Program.Multiply("8", "3"));
+    }
+}
+
+[TestClass]
+public class Division
+{
+	[TestMethod]
+	public void Divide_Valid()
+	{
+        Assert.AreEqual(10, Program.Divide("10", "1"));
+        Assert.AreEqual(0, Program.Divide("0", "1"));
+        Assert.AreEqual(1, Program.Divide("55", "55"));
+    }
+
+}
+
+[TestClass]
+public class Power
+{
+	[TestMethod]
+	public void Power_Valid()
+	{
+        Assert.AreEqual(2, Program.Power("2", "1"));
+        Assert.AreEqual(4, Program.Power("2", "2"));
+        Assert.AreEqual(1, Program.Power("15", "0"));
+    }
+}
